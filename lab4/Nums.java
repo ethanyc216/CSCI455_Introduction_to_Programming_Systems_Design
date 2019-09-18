9e106082ad88d0e08d52b8a2113d9bb4df5bacd0
student <ychen865@usc.edu>
2019-09-17 17:24:29 -0700

    add lab4

---
+++ lab4/Nums.java
@@ -0,0 +1,62 @@
+
+/**
+   Stores a sequence of integer data values and supports some computations
+   with it.
+
+   CS 455 Lab 4.
+*/
+public class Nums {
+
+
+   /**
+      Create an empty sequence of nums.
+   */
+   public Nums () {
+
+   }
+
+   /**
+      Add a value to the end of the sequence.
+   */
+   public void add(int value) {
+
+   }
+
+
+   /**
+      Return the minimum value in the sequence.
+      If the sequence is empty, returns Integer.MAX_VALUE
+   */
+   public int minVal() {
+
+      return 0;    // stub code to get it to compile
+
+   }
+
+   /**
+      Prints out the sequence of values as a space-separated list 
+      on one line surrounded by parentheses.
+      Does not print a newline.
+      E.g., "(3 7 4 10 2 7)", for empty sequence: "()"
+   */
+   public void printVals() {
+
+   }
+
+   /**
+      Returns a new Nums object with all the values from this Nums
+      object that are above the given threshold.  The values in the
+      new object are in the same order as in this one.
+      E.g.: call to myNums.valuesGT(10) where myNums = (3 7 19 4 21 19 10)
+      returns      (19 21 19)
+      myNums after call:  (3 7 19 4 21 19 10)
+      The method does not modify the object the method is called on.
+   */
+   public Nums valuesGT(int threshold) {
+
+      return new Nums();  // stub code to get it to compile
+
+   }
+
+    
+}
